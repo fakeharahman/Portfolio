9594794f56ab5b8a984505e6ba1d89e94a06f56b
Fakeha Rahman <fakeha126@gmail.com>
2020-07-20 19:27:47 +0530

    builld

---
+++ build/precache-manifest.4d807edac2da691e7d9ee8227656de54.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd2092fc8d3f7691e3201cee94221f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "879e46a86d384aca4476",
+    "url": "/static/css/2.18bc7be3.chunk.css"
+  },
+  {
+    "revision": "3eaa31885c7a6d530ab4",
+    "url": "/static/css/3.68e3ce4b.chunk.css"
+  },
+  {
+    "revision": "1aa51d77c0951de05602",
+    "url": "/static/css/main.f491d664.chunk.css"
+  },
+  {
+    "revision": "879e46a86d384aca4476",
+    "url": "/static/js/2.96a55f07.chunk.js"
+  },
+  {
+    "revision": "36bd8d2ec889dd3d833259ab5de4fbd7",
+    "url": "/static/js/2.96a55f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eaa31885c7a6d530ab4",
+    "url": "/static/js/3.ae951daf.chunk.js"
+  },
+  {
+    "revision": "1aa51d77c0951de05602",
+    "url": "/static/js/main.4d3ecf3f.chunk.js"
+  },
+  {
+    "revision": "531399b1eefb252d88a5",
+    "url": "/static/js/runtime-main.3f3952e1.js"
+  },
+  {
+    "revision": "fc39a631360a14162e5cad30d3d8a8d1",
+    "url": "/static/media/envelope-line-black.fc39a631.svg"
+  },
+  {
+    "revision": "d2186505eafed41c0769e888d151996e",
+    "url": "/static/media/envelope-line.d2186505.svg"
+  },
+  {
+    "revision": "592e3b3166ed1f8b2732af8c8b41e0bb",
+    "url": "/static/media/undraw_content_team_3epn (2).592e3b31.svg"
+  },
+  {
+    "revision": "56cffff0de21964b482c2ecd4df72122",
+    "url": "/static/media/undraw_design_components_9vy6.56cffff0.svg"
+  },
+  {
+    "revision": "d8bb3ae4ef12835828564961c4dad027",
+    "url": "/static/media/undraw_feedback_h2ft.d8bb3ae4.svg"
+  },
+  {
+    "revision": "2e29bb4ab01abc6bc6a81b3d50f64310",
+    "url": "/static/media/undraw_proud_coder_7ain (1).2e29bb4a.svg"
+  },
+  {
+    "revision": "13af27a9d691f81e5a1092cfce7d6d63",
+    "url": "/static/media/woman.13af27a9.jpg"
+  }
+]);
