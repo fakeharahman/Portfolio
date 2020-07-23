cb14ec510925e7a16be51d439521fc4f56283b82
Fakeha Rahman <fakeha126@gmail.com>
2020-07-24 03:04:06 +0530

    build

---
+++ build/precache-manifest.023db76719c9bfb5c5847194f83756fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438fbc928f80793812e491f57edce4a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ddcb4448056798c2e1",
+    "url": "/static/css/2.18bc7be3.chunk.css"
+  },
+  {
+    "revision": "637393b821cce60b00d0",
+    "url": "/static/css/3.9b28d3f8.chunk.css"
+  },
+  {
+    "revision": "7f085264333722dcf042",
+    "url": "/static/css/main.ddb4a78c.chunk.css"
+  },
+  {
+    "revision": "69ddcb4448056798c2e1",
+    "url": "/static/js/2.2826a848.chunk.js"
+  },
+  {
+    "revision": "36bd8d2ec889dd3d833259ab5de4fbd7",
+    "url": "/static/js/2.2826a848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637393b821cce60b00d0",
+    "url": "/static/js/3.92e988c8.chunk.js"
+  },
+  {
+    "revision": "7f085264333722dcf042",
+    "url": "/static/js/main.ecb3b5fa.chunk.js"
+  },
+  {
+    "revision": "d187085ea96db99c7b3e",
+    "url": "/static/js/runtime-main.9193b0c7.js"
+  },
+  {
+    "revision": "017901701b0f489fab6fe3d577bb02d7",
+    "url": "/static/media/design.01790170.jpg"
+  },
+  {
+    "revision": "1605a3e3d14b01fb8ba7af4377735fee",
+    "url": "/static/media/develop.1605a3e3.jpg"
+  },
+  {
+    "revision": "fc39a631360a14162e5cad30d3d8a8d1",
+    "url": "/static/media/envelope-line-black.fc39a631.svg"
+  },
+  {
+    "revision": "cc802f449f08c6dba66cfbe87df10168",
+    "url": "/static/media/envelope-line.cc802f44.svg"
+  },
+  {
+    "revision": "3266cd4ea8034c3c7a6e9281068cbe62",
+    "url": "/static/media/feedback.3266cd4e.svg"
+  },
+  {
+    "revision": "5ee39bcadc9af58694b38245efc89842",
+    "url": "/static/media/glasses-laptop.5ee39bca.jpg"
+  },
+  {
+    "revision": "7df1c9b231ee88b05052d695dca92fb9",
+    "url": "/static/media/left-arrow.7df1c9b2.svg"
+  },
+  {
+    "revision": "b11f585bd8ddef832e6e9648378bc1eb",
+    "url": "/static/media/right-arow.b11f585b.svg"
+  },
+  {
+    "revision": "13af27a9d691f81e5a1092cfce7d6d63",
+    "url": "/static/media/woman.13af27a9.jpg"
+  }
+]);
